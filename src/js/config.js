684051612933f48457b1fc60ac3a2812c573a404
= <=>
2021-12-12 13:56:25 +0530

    Initial commitl

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e89c3e70-a2db-44bf-88d0-0e0da87952cd';
+export const MODAL_CLOSE_SEC = 2.5;
